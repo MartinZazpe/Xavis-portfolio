b5db066e8edd8044f0ddaca375d48fe3c9a70636
MartinZazpe <martinzazpe1@gmail.com>
2022-11-27 20:21:25 +0100

    adding back all routes, without images still, but should display JS & css

--- api/mainRoutes.js
+++ api/mainRoutes.js
@@ -3,28 +3,28 @@ var router = express.Router()
 var indexController = require('../controllers/index')
 
 /* GET home page test.  */
-router.get('/', function (req, res, next) {
-    res.render('home', { title: 'Express' })
-})
+// router.get('/', function (req, res, next) {
+//     res.render('home', { title: 'Express' })
+// })
 
 
 /* GET home page. */
 
-// router.get('/', indexController.home)
+router.get('/', indexController.home)
 
-// router.get('/headshots', indexController.headshots)
+router.get('/headshots', indexController.headshots)
 
-// /* resume view */
-// router.get('/myResume', indexController.myResume)
+/* resume view */
+router.get('/myResume', indexController.myResume)
 
-// /*resume download */
-// router.get('/resumeDownload', indexController.myResumeDownload)
+/*resume download */
+router.get('/resumeDownload', indexController.myResumeDownload)
 
-// router.get('/myStory', indexController.myStory)
+router.get('/myStory', indexController.myStory)
 
-// router.get('/film&Tv', indexController.myWorks)
+router.get('/film&Tv', indexController.myWorks)
 
-// router.get('/contact', indexController.contact)
+router.get('/contact', indexController.contact)
 
 
 
